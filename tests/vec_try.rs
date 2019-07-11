2d0ceaf2f07b048b8498952f1b0d7d9702f462a0
Andreas Molzer <andreas.molzer@gmx.de>
2019-07-11 20:11:10 +0200

    Make try_reserve an explicit nightly feature
    Correctly annotates the respective test that requires it as such in the
    `Cargo.toml` file. The structure of CI-tests already enables all
    features on nightly such that that test is ran only there.

--- tests/vec_try.rs
+++ tests/vec_try.rs
@@ -1,11 +1,10 @@
-#![cfg_attr(feature = "try_reserve", feature(try_reserve))]
+#![feature(try_reserve)]
 
 use static_alloc::Slab;
 
 #[global_allocator]
 static A: Slab<[u8; 1 << 16]> = Slab::uninit();
 
-#[cfg(feature = "try_reserve")]
 #[test]
 fn vec_fail_reserve() {
     let mut v: Vec<u8> = Vec::new();
