4e29a0ae2002ef6ad0dbdba382112192d05c5962
Andreas Molzer <andreas.molzer@gmx.de>
2019-07-11 21:31:07 +0200

    PoC for a large static in a small elf file
    Disabled by default because of resource exhaustion, obviously.

---
+++ tests/huuuuuge.rs
@@ -0,0 +1,16 @@
+use static_alloc::Slab;
+
+// That's 1 GB. Only rustc/llvm chokes, the `elf` binary itself does not grow. It is up to the
+// loader to actually provide that data to our program.
+#[global_allocator]
+static OMG: Slab<[u8; 1 << 30]> = Slab::uninit();
+
+#[test]
+fn ok_vec() {
+    let v = vec![0xdeadbeef_u32; 1 << 26];
+    v.into_iter()
+        .for_each(|x| assert_eq!(x, 0xdeadbeef_u32));
+    // If you choose to execute it, you have time to view the program in `top` or w/e.
+    std::thread::sleep_ms(10000);
+}
+
