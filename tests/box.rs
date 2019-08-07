440ee2090e494b1f408ab402dc6a6f41aaaf89be
Andreas Molzer <andreas.molzer@gmx.de>
2019-08-07 15:52:19 +0200

    Add explicit recursive list Box test

---
+++ tests/box.rs
@@ -0,0 +1,28 @@
+use static_alloc::boxed::Box;
+use static_alloc::Slab;
+
+#[test]
+fn recursive() {
+    enum List<'a, T> {
+        Nil,
+        Cons(T, Box<'a, Self>),
+    }
+
+    impl<T> List<'_, T> {
+        pub fn len(&self) -> usize {
+            match self {
+                List::Nil => 0,
+                List::Cons(_, tail) => 1 + tail.len(),
+            }
+        }
+    }
+
+    let slab: Slab<[usize; 32]> = Slab::uninit();
+    let mut list = slab.boxed(List::Nil).unwrap();
+
+    for i in 0usize..8 {
+        list = slab.boxed(List::Cons(i, list)).unwrap();
+    }
+
+    assert_eq!(list.len(), 8);
+}
