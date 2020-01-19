baa284418cd1a38a22c2a8a3794f0d22c877dc1f
Andreas Molzer <andreas.molzer@gmx.de>
2020-01-19 15:00:35 +0100

    Add the basic core implementations for fill

--- fill/src/lib.rs
+++ fill/src/lib.rs
@@ -3,10 +3,18 @@
 #[cfg(feature = "alloc")]
 extern crate alloc;
 
-/// Fill a collection from an iterator and return remaining iterator.
+/// Fill a collection from an iterator until a capacity has been reached.
 pub trait Fill<T> {
-    fn fill<I>(&mut self, iter: I) -> I::IntoIter
+    fn fill<I>(&mut self, iter: I)
         where I: IntoIterator<Item=T>;
+
+    fn fill_and_spill<I>(&mut self, iter: I) -> I::IntoIter
+        where I: IntoIterator<Item=T>
+    {
+        let mut iter = iter.into_iter();
+        self.fill(&mut iter);
+        iter
+    }
 }
 
 #[cfg(feature = "alloc")]
@@ -15,11 +23,9 @@ impl<T> Fill<T> for alloc::vec::Vec<T> {
     fn fill<I>(&mut self, iter: I) -> I::IntoIter
         where I: IntoIterator<Item=T>
     {
-        let mut iter = iter.into_iter();
-        for item in iter.by_ref().take(self.capacity() - self.len()) {
+        for item in iter.take(self.capacity() - self.len()) {
             self.push(item);
         }
-        iter
     }
 }
 
@@ -29,10 +35,97 @@ impl<T> Fill<T> for alloc::collections::VecDeque<T> {
     fn fill<I>(&mut self, iter: I) -> I::IntoIter
         where I: IntoIterator<Item=T>
     {
-        let mut iter = iter.into_iter();
-        for item in iter.by_ref().take(self.capacity() - self.len()) {
+        for item in iter.take(self.capacity() - self.len()) {
             self.push_back(item);
         }
-        iter
+    }
+}
+
+/// Consumes all items until the first `None`.
+impl Fill<()> for () {
+    fn fill<I>(&mut self, iter: I)
+        where I: IntoIterator<Item=()>
+    {
+        for () in iter { }
+    }
+}
+
+/// If the option does not yet contain an item, insert the next item of the iterator.
+///
+/// Note that this will call `iter.next()` at most once. The option will still be empty if the
+/// iterator is not fused and returns `None` as the first result of that call.
+impl<T> Fill<T> for Option<T> {
+    fn fill<I>(&mut self, iter: I)
+        where I: IntoIterator<Item=T>
+    {
+        let mut iter = iter.into_iter();
+        match self {
+            None => *self = iter.next(),
+            Some(_) => (),
+        }
+    }
+}
+
+/// Tries to fill the container in the `Ok`.
+///
+/// A result is full if it contains an error, or if the container in `Ok` is full.
+impl<C, E, T> Fill<Result<T, E>> for Result<C, E>
+    where C: Fill<T>,
+{
+    fn fill<I>(&mut self, iter: I)
+        where I: IntoIterator<Item=Result<T, E>>
+    {
+        let mut iter = iter.into_iter();
+
+        let container = match self {
+            Ok(container) => container,
+            Err(_) => return,
+        };
+
+        let err = loop {
+            match iter.next() {
+                None => return,
+                Some(Err(err)) => break err,
+                Some(Ok(ok)) => {
+                    container.fill(core::iter::once(ok));
+                },
+            };
+        };
+
+        *self = Err(err);
+    }
+}
+
+/// Advances self while inserting elements from the iterator.
+impl<T> Fill<T> for core::slice::IterMut<'_, T> {
+    fn fill<I>(&mut self, iter: I)
+        where I: IntoIterator<Item=T>
+    {
+        let mut iter = iter.into_iter();
+        // Do not use `zip` to control the exact calls to `next`.
+        while self.len() != 0 {
+            if let Some(item) = iter.next() {
+                *self.next().unwrap() = item;
+            } else {
+                break
+            }
+        }
+    }
+}
+
+/// Advances self while swapping elements with the iterator.
+impl<'a, T: 'a> Fill<&'a mut T> for core::slice::IterMut<'_, T> {
+    fn fill<I>(&mut self, iter: I)
+        where I: IntoIterator<Item=&'a mut T>
+    {
+        let mut iter = iter.into_iter();
+        // Do not use `zip` to control the exact calls to `next`.
+        while self.len() != 0 {
+            if let Some(item) = iter.next() {
+                core::mem::swap(self.next().unwrap(), item);
+            } else {
+                break
+            }
+        }
     }
 }
---
+++ fill/tests/core.rs
@@ -0,0 +1,46 @@
+use fill::Fill;
+
+#[test]
+fn slice() {
+    let mut memory = [0u8; 8];
+    let tail = memory.iter_mut().fill_and_spill(0..);
+
+    assert_eq!(memory, [0, 1, 2, 3, 4, 5, 6, 7]);
+    assert_eq!(tail.start, 8);
+}
+
+#[test]
+fn slice_and_swap() {
+    let mut zero = [0u8; 2];
+    let mut ones = [1u8; 4];
+
+    zero.iter_mut().fill(ones.iter_mut());
+    assert_eq!(zero, [1, 1]);
+    assert_eq!(ones, [0, 0, 1, 1]);
+}
+
+#[test]
+fn option() {
+    let mut slot = None;
+    slot.fill(Some(42));
+    assert_eq!(slot, Some(42));
+
+    slot.fill(Some(0xFF));
+    assert_eq!(slot, Some(42));
+}
+
+#[test]
+fn result() {
+    let mut memory = [0u8; 4];
+    let mut free = Ok(memory.iter_mut());
+
+    let unfilled = free.fill_and_spill((0..2).map(Ok));
+    assert_eq!(free.as_ref().ok().unwrap().len(), 2);
+    assert_eq!(unfilled.len(), 0);
+
+    let unfilled = free.fill_and_spill(vec![Err(0xee), Ok(4)]);
+    assert_eq!(free.err(), Some(0xee));
+    assert_eq!(unfilled.len(), 1);
+
+    assert_eq!(memory, [0, 1, 0, 0]);
+}
