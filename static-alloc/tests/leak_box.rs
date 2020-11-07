58fa6c84ef0ccf23e1286acecd98a5509d8ebba6
Andreas Molzer <andreas.molzer@gmx.de>
2020-11-07 03:47:12 +0100

    Add integration tests for LeakBox

---
+++ static-alloc/tests/leak_box.rs
@@ -0,0 +1,35 @@
+use core::cell::RefCell;
+use static_alloc::Bump;
+
+#[test]
+fn leak_box_drops() {
+    let bump: Bump<[*const (); 4]> = Bump::uninit();
+    let cell = RefCell::new(());
+    assert!(cell.try_borrow().is_ok());
+
+    let _ = bump.boxed(cell.borrow()).unwrap();
+    assert!(cell.try_borrow().is_ok(), "Immediately dropped it");
+    assert!(cell.try_borrow_mut().is_ok(), "Immediately dropped it");
+
+    let boxed = bump.boxed(cell.borrow()).unwrap();
+    assert!(cell.try_borrow().is_ok(), "Borrow works, of course");
+    assert!(cell.try_borrow_mut().is_err(), "Still borrowed");
+    drop(boxed);
+
+    assert!(cell.try_borrow_mut().is_ok(), "No longer borrowed");
+}
+
+#[test]
+fn leaking() {
+    struct PanicOnDrop(usize);
+    impl Drop for PanicOnDrop {
+        fn drop(&mut self) {
+            panic!("Do not drop me.");
+        }
+    }
+
+    let bump: Bump<[usize; 1]> = Bump::uninit();
+    let boxed = bump.boxed(PanicOnDrop(0));
+    core::mem::forget(boxed);
+    // Panic averted.
+}
